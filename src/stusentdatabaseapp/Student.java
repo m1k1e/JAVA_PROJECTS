9667b14d1d40fbf8d33f0787eca17d14c5f12765
m1k1e <mikle.fedorenko@gmail.com>
2018-06-15 18:05:32 +0300

    User can enter first name, last name and grade year. Application output this data to console.

--- src/stusentdatabaseapp/Student.java
+++ src/stusentdatabaseapp/Student.java
@@ -1,5 +1,39 @@
 package stusentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private int studentID;
+	private String courses;
+	private String tuitionBalance;
+	private int costOfCourse = 600;
+	
+	// Constructor: prompt user to enter student`s and year
+	public Student() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter student first name: ");
+		this.firstName = sc.nextLine();
+		
+		System.out.println("Enter student last name: ");
+		this.lastName = sc.nextLine();
+		
+		System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+		this.gradeYear = sc.nextInt();
+		
+		System.out.println(firstName + " " + lastName + " " + gradeYear);
+	}
+	// Generate an ID
+	
+	// Enroll an courses
+	
+	// View balance
+	
+	//Pay Tuition
+	
+	//Show status
+	
 
 }
--- src/stusentdatabaseapp/StudentDatabaseApp.java
+++ src/stusentdatabaseapp/StudentDatabaseApp.java
@@ -3,7 +3,11 @@ package stusentdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Student stu1 = new Student();
+		
+		// Ask how many new users we want to add
+		
+		// Create n number of new students
 
 	}
 
