8a14a4813e7886071848bf2c284b2dbabc5be4ce
m1k1e <mikle.fedorenko@gmail.com>
2018-06-15 19:58:15 +0300

    Add function View balance and Pay Tuition

--- src/stusentdatabaseapp/Student.java
+++ src/stusentdatabaseapp/Student.java
@@ -6,10 +6,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentID;
-	private String courses;
-	private String tuitionBalance;
-	private int costOfCourse = 600;
+	private String studentID;
+	private String courses = null;
+	private int tuitionBalance = 0;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 	
 	// Constructor: prompt user to enter student`s and year
 	public Student() {
@@ -23,15 +24,54 @@ public class Student {
 		System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
 		this.gradeYear = sc.nextInt();
 		
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + studentID);
+		
+		
 	}
 	// Generate an ID
-	
+	private void setStudentID() {
+		// Grade level + ID
+		id++;
+		this.studentID = gradeYear + " " + id;
+	}
 	// Enroll an courses
+	public void enroll() {
+		// Get inside a loop, user hits 0
+		
+		do {
+				System.out.println("Enter course to enroll (Q to quit): ");
+				Scanner sc = new Scanner(System.in);
+				String course = sc.nextLine();
+				if(!course.equals("Q")) {
+					courses = courses + "\n" + course;
+					tuitionBalance = tuitionBalance + costOfCourse;
+				} 
+				else 
+				{
+					break;
+					}
+		}while(1 != 0);
+		
+		System.out.println("ENROLLED IN: " + courses);
+	}
 	
 	// View balance
+	public void viewBalance() {
+		System.out.println("Your balance is: $" + tuitionBalance);
+	}
 	
 	//Pay Tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter your payment: $");
+		Scanner sc = new Scanner(System.in);
+		int payment = sc.nextInt(); 
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of $ " + payment);
+		viewBalance();
+	}
 	
 	//Show status
 	
--- src/stusentdatabaseapp/StudentDatabaseApp.java
+++ src/stusentdatabaseapp/StudentDatabaseApp.java
@@ -4,6 +4,8 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		Student stu1 = new Student();
+		stu1.enroll();
+		stu1.payTuition();
 		
 		// Ask how many new users we want to add
 		
