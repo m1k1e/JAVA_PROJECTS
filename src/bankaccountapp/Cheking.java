e3d925d1392934895fe3d690f7b3a8b796b80928
m1k1e <mikle.fedorenko@gmail.com>
2018-06-23 17:04:17 +0300

    add Debit Card functions

--- src/bankaccountapp/Cheking.java
+++ src/bankaccountapp/Cheking.java
@@ -9,15 +9,23 @@ public class Cheking extends Account {
 	public Cheking(String name, String sSN, double initDeposit) {
 		super(name, sSN, initDeposit);
 		accountNumber = "2" + accountNumber;
+		setDebitCard();
 		//System.out.println("Account Number: " + this.accountNumber);
 		//System.out.println("New Checking Account!");
 	}
 	
 // List any methods to the checking account
+	private void setDebitCard() {
+		debitCardNumber = (int)(Math.random() * Math.pow(10, 12));
+		debitCardPIN = (int)(Math.random() * Math.pow(10, 4));
+	}
 	
 	public void showInfo() {
 		super.showInfo();
-		System.out.println("ACCOUNT TYPE: Checking!");
+		System.out.println("Your Checking Account Features" +
+				"\n Debit Card Number: " + debitCardNumber +
+				"\n Debit Card PIN: " + debitCardPIN
+				);
 	}
 	
 }
