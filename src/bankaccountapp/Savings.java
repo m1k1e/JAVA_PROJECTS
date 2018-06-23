ab3664a61dd762c1784a5536116b7a491509cdd9
m1k1e <mikle.fedorenko@gmail.com>
2018-06-23 16:11:11 +0300

    add Safety Deposit functions

--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -9,14 +9,28 @@ public class Savings extends Account {
 public Savings(String name, String sSN, double initDeposit) {
 	super(name, sSN, initDeposit);
 	accountNumber = "1" + accountNumber;
+	
+	setSafetyDepositBox();
 	//System.out.println("Account Number: " + this.accountNumber);
 	//System.out.println("New Savings Account!");
+}
+
+
+private void setSafetyDepositBox() {
+	safetyDepositBoxID = (int) (Math.random() * Math.pow(10, 3));
+	safetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
+	//System.out.println(safetyDepositBoxID);
 }
 // List any methods specific to savings account
 
 public void showInfo() {
 	super.showInfo();
-	System.out.println("ACCOUNT TYPE: Savings!");
+	System.out.println(
+			" Your Savings Account Features" +
+			"\n Safety Deposit Box ID: " + safetyDepositBoxID + 
+			"\n Sefety Deposite Box Key: " + safetyDepositBoxKey
+			);
+	//System.out.println("ACCOUNT TYPE: Savings!");
 }
 
 }
