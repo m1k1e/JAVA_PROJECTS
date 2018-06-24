3cf4c164e0192bba04bb5b73aed080815576f95a
m1k1e <mikle.fedorenko@gmail.com>
2018-06-24 12:14:19 +0300

    Add deposit, withdraw, transfer and printBalace methods

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -14,6 +14,11 @@ public class BankAccountApp {
 	
 	chkacc1.showInfo();
 	
+	
+	savacc1.deposit(5000);
+	savacc1.withdraw(200);
+	savacc1.transfer("Brokerage", 3000);
+	
   // Read a CSV file then create new accounts based on that data 
 
 	}
