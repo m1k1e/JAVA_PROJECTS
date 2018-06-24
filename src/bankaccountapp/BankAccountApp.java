e49427c535e2f7967e9f319bddd794ca7e5b3d37
m1k1e <mikle.fedorenko@gmail.com>
2018-06-24 15:55:25 +0300

    add function reading csv files and creating new accounts

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,10 +1,14 @@
 package bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
 
 	public static void main(String[] args) {
 		
-	Cheking chkacc1 = new Cheking("Tom Wilson", "321456879", 1500);
+	String file = "/home/user/Документи/Java/JAVA_PROJECTS/src/bankaccountapp/NewBankAccounts.csv";
+		
+   /*Cheking chkacc1 = new Cheking("Tom Wilson", "321456879", 1500);
 	
 	Savings savacc1 = new Savings("Rich Lowe", "4566557897", 2500);
 	
@@ -19,10 +23,18 @@ public class BankAccountApp {
 	
 	//savacc1.deposit(5000);
 	//savacc1.withdraw(200);
-	//savacc1.transfer("Brokerage", 3000);
+	//savacc1.transfer("Brokerage", 3000);*/
 	
 	
   // Read a CSV file then create new accounts based on that data 
+	List<String[]> newAccountHolders = utilities.CSV.read(file);
+	for(String[] accountHolder : newAccountHolders) {
+		System.out.println("New Account: ");
+		System.out.println(accountHolder[0]);
+		System.out.println(accountHolder[1]);
+		System.out.println(accountHolder[2]);
+		System.out.println(accountHolder[3]);
+	}
 
 	}
 
