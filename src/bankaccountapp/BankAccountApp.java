bac4ada6a94345e14fd67c8c3344fe175da68f79
m1k1e <mikle.fedorenko@gmail.com>
2018-06-24 17:40:26 +0300

    final application Bank Account

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,12 +1,13 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
 	public static void main(String[] args) {
 		
-	String file = "/home/user/Документи/Java/JAVA_PROJECTS/src/bankaccountapp/NewBankAccounts.csv";
+	List<Account> accounts = new LinkedList<Account>();
 		
    /*Cheking chkacc1 = new Cheking("Tom Wilson", "321456879", 1500);
 	
@@ -27,6 +28,7 @@ public class BankAccountApp {
 	
 	
   // Read a CSV file then create new accounts based on that data 
+	String file = "/home/user/Документи/Java/JAVA_PROJECTS/src/bankaccountapp/NewBankAccounts.csv";
 	List<String[]> newAccountHolders = utilities.CSV.read(file);
 	for(String[] accountHolder : newAccountHolders) {
 		//System.out.println("New Account: ");
@@ -35,12 +37,15 @@ public class BankAccountApp {
 		String accountType = accountHolder[2];
 		double initDeposite = Double.parseDouble(accountHolder[3]);
 		
-		System.out.println(name + " " + sSN + " " + accountType + " $" + initDeposite);
+		//System.out.println(name + " " + sSN + " " + accountType + " $" + initDeposite);
+	
 		if (accountType.equals("Savings")) {
-			System.out.println("Open a Savings Account!");
+			//System.out.println("Open a Savings Account!");
+			accounts.add(new Savings(name, sSN, initDeposite));
 		}
 		else if (accountType.equals("Checking")) {
-			System.out.println("Open a Chacking Account!");
+			//System.out.println("Open a Chacking Account!");
+			accounts.add(new Cheking(name, sSN, initDeposite));
 		}
 		else {
 			System.out.println("Error readin Account Type!");
@@ -51,7 +56,15 @@ public class BankAccountApp {
 		System.out.println(accountHolder[2]);
 		System.out.println(accountHolder[3]);*/
 	}
+	
+	//accounts.get(5).showInfo();
+	for (Account acc : accounts) {
+		System.out.println("\n****************");
+		acc.showInfo();
+	}
 
 	}
+	
+	
 
 }
